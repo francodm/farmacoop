90080ae312604cdb290165eb865e0bfcf81d745f
Catalina Syddall <catalinasyddall@gmail.com>
2019-10-15 16:12:35 -0300

    first commit

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+var prevScrollpos = window.pageYOffset;
+var navbar = document.getElementById("navbar")
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+
+  if (currentScrollPos > 700){
+      navbar.classList.add("visible")
+      navbar.classList.remove("transparent")
+  } else {
+      navbar.classList.add("transparent")
+  }
+} 
